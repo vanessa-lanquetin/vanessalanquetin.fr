9c14e4754ec36391faa90cb7cbf346417347221b
Vanessa Lanquetin <vanessa.lanquetin88@gmail.com>
2021-05-26 18:17:34 +0200

    chore(stack-monitor): add git home and git remote to stack-monitor

--- stack.js
+++ stack.js
@@ -1,30 +1,46 @@
 const stack = [
   {
-    label: 'vanessalanquetin.fr-server',
-    url: "http://localhost:8080",
-    spawnCmd: 'npm',
-    spawnArgs: ['run', 'server'],
-    spawnOptions: {
-      cwd:  __dirname,
-      env: Object.assign({
-        PORT: '4215',
-        EMAIL: '',
-        PASSWORD: '',
-      }, process.env)
-    }
-  },
-  {
-    label: 'vanessalanquetin.fr-front',
-    spawnCmd: 'npm',
+    label: "vanessalanquetin.fr-server",
+    git: {
+      home: "https://github.com/vanessouille/vanessalanquetin.fr",
+      remote:
+        "https://github.com/vanessouille/vanessalanquetin.fr.git",
+    },
     url: "http://localhost:4215",
-    spawnArgs: ['run', 'client'],
+    spawnCmd: "npm",
+    spawnArgs: ["run", "server"],
     spawnOptions: {
       cwd: __dirname,
-      env: Object.assign({
-        VUE_APP_SERVER_URL: 'http://localhost',
-        VUE_APP_SERVER_PORT: 4215,
-      }, process.env)
-    }
+      env: Object.assign(
+        {
+          PORT: "4215",
+          EMAIL: "",
+          PASSWORD: "",
+        },
+        process.env
+      ),
+    },
   },
-]
+  {
+    label: "vanessalanquetin.fr-front",
+    git: {
+      home: "https://github.com/vanessouille/vanessalanquetin.fr",
+      remote:
+        "https://github.com/vanessouille/vanessalanquetin.fr.git",
+    },
+    spawnCmd: "npm",
+    url: "http://localhost:8080",
+    spawnArgs: ["run", "client"],
+    spawnOptions: {
+      cwd: __dirname,
+      env: Object.assign(
+        {
+          VUE_APP_SERVER_URL: "http://localhost",
+          VUE_APP_SERVER_PORT: 4215,
+        },
+        process.env
+      ),
+    },
+  },
+];
 module.exports = stack
