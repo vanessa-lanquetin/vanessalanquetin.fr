0c05f10d3cf48039a7714cef371fc85cd9061cb3
Vanessa Lanquetin <vanessa.lanquetin88@gmail.com>
2021-06-12 21:49:04 +0200

    update cv

---
+++ src/views/CV copy.vue
@@ -0,0 +1,169 @@
+<template>
+<div id="global">
+    <section id="section1">
+      <section class="container" id="container-presentation">
+        <h3  id="infos" >Présentation</h3> 
+        <div class="text-container">
+        <ul>
+          <li>Age:23 ans</li>
+          <li>Localisation:Neuves-Maisons</li>
+        </ul>
+        </div>            
+      </section>
+      <section class="container" id="container-formations">
+        <h3 id="formations">Formations</h3>   
+        <div class="text-container">
+        <ul>
+          <li>Formation Développement en autodicadacte – Openclassrooms, 2021</li>
+          <li>Formation Objectif Alternance – Afpa, 2020</li>
+          <li>BTS Assistant Manager (niveau) – Lycée Louis Lapicque, 2017</li>
+          <li>BAC Gestion Administration – Lycée Isabelle Viviani, 2015</li>
+          <li>BEP Métiers Services Administratif – Lycée Isabelle Viviani, 2014</li>
+        </ul>
+        </div>            
+      </section>  
+    </section>
+    <section id="section2">
+      <section class="container" id="container-stages">
+        <h3 id="stages">Stages</h3>   
+        <div class="text-container">
+        <ul>
+          <li>Commerciale, O2 CARES SERVICES - 1 mois, 2019</li>
+          <li>Assistante de direction, LES RESTOS DU COEURS - 1 mois, 2017</li>
+          <li>Assistante Service Qualité, EUROVITA - 1 mois, 2016</li>
+          <li>Assistante administrative, BIBLIOTHEQUE DE VITTEL - 1 mois, 2015</li>
+          <li>Assistante administrative, PHARMACIE DES SOURCES - 1 mois, 2014</li>
+          <li>Assistante du Président, ADMR - 1 mois, 2014</li>
+          <li>Assistante-adjoint, CARREFOUR MARKET - 1 mois, 2014</li>
+          <li>Assistante de direction, BIBLIOTHEQUE DE VITTEL - 1 mois, 2013</li>
+          <li>Assistante Commerciale, MMA ASSURANCES - 1 mois, 2013</li>
+        </ul>
+        </div>
+          <div id="container-loisirs" >
+        <h3 id="text-loisirs">Centres d’intérêts</h3>
+        <div class="text-container">
+          <ul>
+            <li>Dessin</li>
+            <li>Cuisine</li>
+            <li>Lecture</li>
+          </ul>
+        </div>
+        </div>   
+      </section>
+
+
+
+      <section class="container" id="container-comp-loisirs">
+        <div id="container-comp">
+        <h3 id="comp">Compétences</h3>   
+        <div class="text-container">
+          <ul>
+            <li>Administratif</li>
+            <li>Commercial</li>
+            <li>Comptabilité </li>
+            <li>Gestion Administrative</li>
+          </ul>
+
+          <h3>Langues</h3>
+            <div class="text-container">
+              <ul>
+                <li>Anglais – Niveau A2</li>
+                <li>Espagnol – Niveau A1</li>
+              </ul> 
+            </div>
+          
+          
+          <h3>Languages</h3>
+            <div class="text-container">
+            <ul>
+              <li>HTML5</li>
+              <li>CSS3</li>
+              <li>JAVASCRIPT</li>
+            </ul> 
+            </div>
+
+          
+          <h3>Informatique</h3>
+          <div class="text-container">
+          <ul> 
+            <li>Access</li>
+              <li>Ethnos </li>
+              <li>Freemind</li>
+              <li>Microsoft Pack Office </li>
+              <li>Visual Code</li>
+              <li>Figma</li>
+            </ul>
+          </div>
+        </div>
+        </div>             
+      </section>  
+  </section>
+</div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.container{
+  border: 3px solid #F2ECD8;
+  box-shadow: 0 0 20px 0 #e4e4e4;
+  margin: 20px;
+  font-size: 20px;
+  padding: 20px;
+  background-color: rgba(242, 236, 216, 0.5);
+}
+#global{
+  margin: 80px;
+}
+#section1,#section2{
+  display: flex;
+  width: 100%;
+
+}
+#container-formations{
+  display: flex;
+  flex-grow: 2;
+  flex-direction: column;
+}
+#container-presentation{
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+}
+#container-stages{
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+}
+#container-comp-loisirs{
+  display: flex;
+  flex-grow: 2;
+}
+#container-comp-loisirs{
+  flex-direction: row;
+}
+li{
+  list-style: none;
+}
+#section2{
+  flex-wrap: wrap;
+  height: 200px;
+  margin-bottom: 20px;
+}
+h3{
+  font-size: 30px;
+  color: #A58DB0;
+  font-weight: normal;
+  text-decoration: underline;
+}
+.text-container{
+    font-size: 22px;  
+    color: #78748B;
+    line-height: 30px;
+}
+</style>
--- src/views/CV.vue
+++ src/views/CV.vue
@@ -1,13 +1,368 @@
 <template>
-  <div>CV</div>
+  <div id="global">
+    <section id="container-presentation">
+      <div>
+        <img
+          id="presentation-img"
+          src="@/assets/img/presentation.jpg"
+          alt="presentation"
+        />
+      </div>
+      <div class="text-container" id="text-presentation">
+        <ul>
+          <li><span class="text-strong">Age:</span>23 ans</li>
+          <li><span class="text-strong">Localisation:</span>Neuves-Maisons</li>
+        </ul>
+      </div>
+    </section>
+    <section id="section1">
+      <section id="container-stages">
+        <div>
+          <img id="stages-img" src="@/assets/img/stages.jpg" alt="stages" />
+        </div>
+        <div class="text-container" id="text-stages">
+          <ul>
+            <li>
+              <span class="text-strong">Commerciale</span>, O2 CARES SERVICES -
+              1 mois, 2019
+            </li>
+            <li>
+              <span class="text-strong">Assistante de direction</span>, LES
+              RESTOS DU COEURS - 1 mois, 2017
+            </li>
+            <li>
+              <span class="text-strong">Assistante Service Qualité</span>,
+              EUROVITA - 1 mois, 2016
+            </li>
+            <li>
+              <span class="text-strong">>Assistante administrative</span>,
+              BIBLIOTHEQUE DE VITTEL - 1 mois, 2015
+            </li>
+            <li>
+              <span class="text-strong">Assistante administrative</span>,
+              PHARMACIE DES SOURCES - 1 mois, 2014
+            </li>
+            <li>
+              <span class="text-strong">Assistante du Président</span>, ADMR - 1
+              mois, 2014
+            </li>
+            <li>
+              <span class="text-strong">Assistante-adjoint</span>, CARREFOUR
+              MARKET - 1 mois, 2014
+            </li>
+            <li>
+              <span class="text-strong">Assistante de direction</span>,
+              BIBLIOTHEQUE DE VITTEL - 1 mois, 2013
+            </li>
+            <li>
+              <span class="text-strong">Assistante Commerciale</span>, MMA
+              ASSURANCES - 1 mois, 2013
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <section id="container-formations">
+        <div>
+          <img
+            id="formations-img"
+            src="@/assets/img/formations.jpg"
+            alt="formations"
+          />
+        </div>
+        <div class="text-container" id="text-formations">
+          <ul>
+            <li class="text-strong">
+              Formation Développement en autodicadacte
+            </li>
+            <li>– Openclassrooms, 2021</li>
+            <li class="text-strong">Formation Objectif Alternance</li>
+            <li>– Afpa, 2020</li>
+            <li class="text-strong">BTS Assistant Manager (niveau)</li>
+            <li>– Lycée Louis Lapicque, 2017</li>
+            <li class="text-strong">BAC Gestion Administration</li>
+            <li>– Lycée Isabelle Viviani, 2015</li>
+            <li class="text-strong">BEP Métiers Services Administratif</li>
+            <li>– Lycée Isabelle Viviani, 2014</li>
+          </ul>
+        </div>
+      </section>
+    </section>
+    <section id="container-competences">
+      >
+      <div>
+        <img
+          id="competences-img"
+          src="@/assets/img/competences.jpg"
+          alt="competences"
+        />
+      </div>
+      <div class="text-container" id="text-competences">
+        <div id="list-competences">
+          <div class="competences">Administratif</div>
+          <div class="competences">Commercial</div>
+          <div class="competences">Comptabilité</div>
+          <div class="competences">Gestion Administrative</div>
+        </div>
+      </div>
+    </section>
+
+    <section id="container-langues">
+      <div>
+        <img id="langues-img" src="@/assets/img/langues.jpg" alt="langues" />
+      </div>
+      <div class="text-container" id="text-langues">
+        <div id="list-langues">
+          <ul>
+            <li><span class="text-strong">Anglais</span> – Niveau A2</li>
+            <li><span class="text-strong">Espagnol</span> – Niveau A1</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="container-languages">
+      <div>
+        <img
+          id="languages-img"
+          src="@/assets/img/languages.jpg"
+          alt="languages"
+        />
+      </div>
+      <div class="text-container" id="text-languages">
+        <div id="list-languages">
+          <ul>
+            <li class="text-strong">HTML5</li>
+            <li class="text-strong">CSS3</li>
+            <li class="text-strong">JAVASCRIPT</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="container-informatique">
+      <div>
+        <img
+          id="informatique-img"
+          src="@/assets/img/informatique.jpg"
+          alt="informatique"
+        />
+      </div>
+      <div class="text-container" id="text-informatique">
+        <div id="list-informatique">
+          <ul>
+            <li class="text-strong">Figma</li>
+            <li class="text-strong">Freemind</li>
+            <li class="text-strong">Pack Office</li>
+            <li class="text-strong">Visual Code</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="container-loisirs">
+      <div>
+        <img id="loisirs-img" src="@/assets/img/loisirs.jpg" alt="loisirs" />
+      </div>
+      <div class="text-container" id="text-loisirs">
+        <div id="list-loisirs">
+          <ul>
+            <li class="text-strong">Cuisine</li>
+            <li class="text-strong">Dessin</li>
+            <li class="text-strong">Lectures</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <section id="container-reseaux">
+      <div>
+        <img id="reseaux-img" src="@/assets/img/reseaux.jpg" alt="reseaux" />
+      </div>
+      <div class="text-container" id="text-reseaux">
+        <div id="list-reseaux">
+          <div id="link-github">
+            <router-link
+              to="https://www.github.com/in/vanessa-lanquetin-15742b153"
+            >
+              <img
+                id="github"
+                src="@/assets/img/github.png"
+                alt="github"
+              />
+            </router-link>
+          </div>
+          <div id="link-linkedin">
+            <router-link
+              to="https://www.linkedin.com/in/vanessa-lanquetin-15742b153"
+            >
+              <img
+                id="linkedin"
+                src="@/assets/img/linkedin.jpg"
+                alt="linkedin"
+              />
+            </router-link>
+        </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
+li {
+  list-style: none;
+}
+#global {
+  margin: 30px;
+}
+h3 {
+  font-size: 30px;
+  color: #a58db0;
+  font-weight: normal;
+  padding-left: 40px;
+}
+.text-container {
+  font-size: 20px;
+  color: #4d4a5f;
+  line-height: 30px;
+}
+#container-presentation {
+  display: flex;
+  flex-direction: row;
+}
+img {
+  width: 250px;
+  height: 250px;
+  position: absolute;
+  z-index: 1;
+}
+#text-presentation {
+  position: relative;
+  z-index: 2;
+  top: 66px;
+  left: 220px;
+}
+#container-stages {
+  margin-top: 100px;
+}
+#text-stages {
+  position: relative;
+  z-index: 2;
+  left: 220px;
+  margin-right: 20px;
+}
 
+#text-formations {
+  position: absolute;
+  z-index: 2;
+  top: 450px;
+  display: flex;
+  flex-direction: column;
+  left: 940px;
+}
+#section1 {
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+#formations-img {
+  top: 255px;
+  left: 999px;
+}
+
+.text-strong {
+  font-weight: bold;
+  color: #d8c78f;
+  font-size: 22px;
+}
+#text-competences {
+  position: absolute;
+  z-index: 2;
+  top: 450px;
+  display: flex;
+  top: 960px;
+  left: 20%;
+}
+#competences-img {
+  top: 750px;
+  left: 42%;
+}
+#section3 {
+  margin-top: 2000px;
+}
+#list-competences {
+  display: flex;
+  padding-right: 30px;
+  font-weight: bold;
+  color: #d8c78f;
+  font-size: 22px;
+}
+.competences {
+  padding-right: 130px;
+  margin: auto;
+}
+#langues-img {
+  top: 1000px;
+  left: 300px;
+}
+#text-langues {
+  position: absolute;
+  z-index: 2;
+  top: 1170px;
+  display: flex;
+  left: 300px;
+}
+#text-languages {
+  position: absolute;
+  z-index: 2;
+  top: 1170px;
+  display: flex;
+  left: 700px;
+}
+#languages-img {
+  top: 1000px;
+  left: 660px;
+}
+#informatique-img {
+  top: 1000px;
+  left: 1020px;
+}
+#text-informatique {
+  position: absolute;
+  z-index: 2;
+  top: 1170px;
+  display: flex;
+  left: 1075px;
+}
+#loisirs-img {
+  top: 1250px;
+  left: 400px;
+}
+#text-loisirs {
+  position: absolute;
+  z-index: 2;
+  top: 1410px;
+  display: flex;
+  left: 445px;
+  margin-bottom: 100px;
+}
+#linkedin {
+  top: 1420px;
+  left: 800px;
+  height: 108px;
+  width: 150px;
+}
+#github{
+  position: absolute;
+  z-index: 1;
+  top: 1390px;
+  left: 1050px;
+  height: 128px;
+  width: 130px;
+}
+#reseaux-img {
+  top: 1252px;
+  left: 878px;
+}
 </style>
