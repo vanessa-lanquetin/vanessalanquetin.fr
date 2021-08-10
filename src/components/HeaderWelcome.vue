0e9190eeccffef955ccd751ae283f73be1f09f20
vanessouille <vanessa.lanquetin88@gmail.com>
2021-08-10 23:02:08 +0200

    animation pulse

--- src/components/HeaderWelcome.vue
+++ src/components/HeaderWelcome.vue
@@ -19,16 +19,28 @@ export default {
 <style lang="scss" scoped>
 h1{
   font-size: 4em;
+  padding:10px;
   text-align: center;
   color: white;
+  border-radius: 10px;
+  height: 130px;
   font-family:"MonteCarlo-Regular",sans-serif;
+  box-shadow: 0 0 0 0 rgb(255, 255, 255);
+  animation: pulse 2.3s infinite;
 }
+@keyframes pulse {
+    to {
+        box-shadow: 0 0 0 18px rgba(255, 253, 253, 0.01);
+    }
+}
+
 .box-card {
   height: 100vh;
 }
 @media screen and(max-width: 810px) {
   h1{
     transform: scale(0.8) translate(0, -40%);  
+    animation:none;
   }
 }
 @media screen and(max-width: 499px) {
