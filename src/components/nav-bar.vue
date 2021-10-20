9c0f6174634f02809f227debc22f4be066ddac84
vanessouille <vanessa.lanquetin88@gmail.com>
2021-10-20 10:39:12 +0200

    nav-bar responsive

--- src/components/nav-bar.vue
+++ src/components/nav-bar.vue
@@ -1,24 +1,54 @@
 <template>
-  <div class="nav" v-scroll-reveal="{preset: 'left'}">
-    <ul id="nav-list">
-      <li><a class="item-nav" href="#presentation">Présentation</a></li>
-      <li><a class="item-nav" href="#competences">Compétences</a></li>
-      <li><a class="item-nav" href="#formations">Formations</a></li>
-      <li><a class="item-nav" href="#hobbies">Centres d'intérêts</a></li>
-      <li><a class="item-nav" href="#contact">Contact</a></li>
-    </ul>
-  </div>
+  <nav>
+    <a class="container-item" href="">
+      <i class="fas fa-graduation-cap"></i>
+      <div class="nav-title-item">CV</div>
+    </a>
+    <a class="container-item" href="">
+      <i class="far fa-heart"></i>
+      <div class="nav-title-item">Lettre de motivation</div>
+    </a>
+    <a class="container-item" href="">
+      <i class="fas fa-desktop"></i>
+      <div class="nav-title-item">Portfolio</div>
+    </a>
+    <a class="container-item" href="">
+      <i class="fas fa-address-book"></i>
+      <div class="nav-title-item">Contact</div>
+    </a>
+  </nav>
 </template>
 
 <script>
 </script>
 
 <style lang="scss" scoped>
-#nav-list {
+  nav {
+    background: rgba(255, 255, 255, 0.1);
+    backdrop-filter: blur(20px);
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    color: #fff;
+  }
+nav,
+#nav-list-desktop,
+#nav-list-mobile {
   width: 100%;
   backdrop-filter: blur(20px);
   display: flex;
   justify-content: space-around;
+}
+#nav-list-mobile {
+  display: none;
+}
+.container-item {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10px;
+  font-size: 20px;
+  padding: 10px;
+;
 }
 .item-nav {
   font-size: 1.2em;
@@ -28,6 +58,23 @@
   &:hover {
     font-size: 1.4em;
     font-weight: bold;
+  }
+}
+@media screen and (max-width: 690px) {
+  .container-item div {
+    display: none;
+  }
+  .container-item {
+    border-bottom: none;
+  }
+  nav {
+    background: rgba(59, 59, 59, 0.993);
+    backdrop-filter: blur(20px);
+    border: 1px solid rgba(255, 255, 255, 0.1);
+    color: #fff;
+    bottom: 0;
+    position: fixed;
+    z-index: 20;
   }
 }
 </style>
