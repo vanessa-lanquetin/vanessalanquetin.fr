594567f7f1800810770c77f70e0185d1e5713c7e
vanessouille <vanessa.lanquetin88@gmail.com>
2021-10-26 17:22:54 +0200

    responsive page contact.vue

--- src/components/contact-component.vue
+++ src/components/contact-component.vue
@@ -1,47 +1,54 @@
 <template>
-    <section class="sec-05" v-scroll-reveal="{preset: 'bottom', interval: 200}">
-      <div class="container">
-        <h2 id="contact" class="section-title" v-scroll-reveal="{preset: 'left'}">Contact</h2>
-        <div class="content">
-          <div class="text-box" v-scroll-reveal="{preset: 'right', delay: 700}">
-            <form action="mailto:lanquetin.vanessa@gmail.com" method="post">
-              <div class="items-contact">
-                <label for="name">Nom :</label>
-                <input type="text" id="name" name="name" required />
-              </div>
-              <div class="items-contact">
-                <label for="mail">Mail :</label>
-                <input type="email" id="mail" name="email" required />
-              </div>
-              <div class="items-contact">
-                <label for="msg">Message :</label>
-                <textarea
-                  id="msg"
-                  name="user_message"
-                  rows="6"
-                  unset
-                  required
-                ></textarea>
-              </div>
-              <div class="button">
-                <button type="submit">Envoyer</button>
-              </div>
-            </form>
-          </div>
+  <section class="sec-05" v-scroll-reveal="{ preset: 'bottom', interval: 200 }">
+    <div class="container">
+      <h2
+        id="contact"
+        class="section-title"
+        v-scroll-reveal="{ preset: 'left' }"
+      >
+        Contact
+      </h2>
+      <div class="content">
+        <div class="text-box" v-scroll-reveal="{ preset: 'right', delay: 700 }">
+          <form action="mailto:lanquetin.vanessa@gmail.com" method="post">
+            <div class="items-contact">
+              <label for="name">Nom :</label>
+              <input type="text" id="name" name="name" required />
+            </div>
+            <div class="items-contact">
+              <label for="mail">Mail :</label>
+              <input type="email" id="mail" name="email" required />
+            </div>
+            <div class="items-contact">
+              <label for="msg">Message :</label>
+              <textarea
+                id="msg"
+                name="user_message"
+                rows="6"
+                unset
+                required
+              ></textarea>
+            </div>
+            <div class="button">
+              <button type="submit">Envoyer</button>
+            </div>
+          </form>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
 .sec-05 .content {
   justify-content: center;
+}
+.sec-05 h2{
+  margin-top: 50px;
 }
 .sec-05 .text-box {
   max-width: none;
@@ -49,7 +56,7 @@ export default {
 }
 .items-contact {
   display: flex;
-  gap: 100px;
+  gap: 50px;
   justify-content: space-between;
 }
 .sec-05 .button {
@@ -74,8 +81,82 @@ textarea {
   max-width: 826px;
   border: 1px solid rgba(255, 255, 255, 0.1);
 }
-#msg{
+#msg {
   min-height: 290px;
 }
+@media screen and (max-width: 1448px) {
+  input,
+  textarea {
+    min-width: 650px;
+  }
+}
+@media screen and (max-width: 1195px) {
+  .sec-05 .text-box {
+    padding: 50px;
+  }
+  .items-contact[data-v-6599f3b0] {
+    gap: 20px;
+  }
+  input,
+  textarea {
+    min-width: 626px;
+  }
+}
+@media screen and (max-width: 867px) {
+  .sec-05 .text-box {
+    padding: 50px;
+  }
+  .items-contact {
+    gap: 10px;
+  }
+  input,
+  textarea {
+    min-width: 500px;
+  }
+}
+@media screen and (max-width: 777px) {
+  .sec-05 .text-box {
+    text-align: left;
+  }
+  .items-contact[data-v-6599f3b0] {
+    display: flex;
+    flex-direction: column;
+  }
+}
+@media screen and (max-width: 649px) {
+  .sec-05 .text-box {
+    padding: 20px;
+  }
+  input,
+  textarea {
+    min-width: 500px;
+  }
+}
+@media screen and (max-width: 591px) {
+  .sec-05 .text-box {
+    padding: 20px;
+  }
+  input,
+  textarea {
+    min-width: 400px;
+  }
+  .sec-05{
+    margin-bottom: 100px;
+  }
+}
 
+@media screen and (max-width: 430px) {
+
+  input,
+  textarea {
+    min-width: 260px;
+  }
+}
+@media screen and (max-width: 323px) {
+
+  input,
+  textarea {
+    min-width: 200px;
+  }
+}
 </style>
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,13 +1,11 @@
 <template>
   <contact-component/>
-  <arrow/>
 </template>
 
 <script>
-import Arrow from '../components/arrow.vue'
 import contactComponent from '../components/contact-component.vue'
 export default {
-  components: { contactComponent, Arrow },
+  components: { contactComponent },
 
 }
 </script>
