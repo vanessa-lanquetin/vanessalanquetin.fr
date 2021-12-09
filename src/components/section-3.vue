95ee9e4e63f720e0e805bd33d009f63a04a5ad2c
vanessa-lanquetin <lanquetin.vanessa@gmail.com>
2021-12-09 16:23:35 +0100

    other test img2

--- src/components/section-3.vue
+++ src/components/section-3.vue
@@ -10,7 +10,7 @@
       </h2>
       <div id="content-formations" class="content">
         <div class="image" v-scroll-reveal="{ preset: 'top', delay: 100 }">
-          <img id="img2" src="../assets/img/img2.webp" alt="jardin botanique" />
+          <img src="../assets/img/portfolio/b2.webp" alt="" />
         </div>
         <div class="info" v-scroll-reveal="{ preset: 'bottom' }">
           <div>
